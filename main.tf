889bcc6f63f3903d242e8cfdb027a4a730058156
Wesley Costa <98036757+FWesleycosta@users.noreply.github.com>
2026-03-04 15:10:11 -0300

    Refactor AWS Step Functions configuration

--- main.tf
+++ main.tf
@@ -1,51 +1,76 @@
-################################################################################
-# AWS Step Functions - State Machine
-################################################################################
+resource "aws_pipe_pipe" "this" {
+  name          = var.pipe_name
+  role_arn      = var.role_arn
+  desired_state = var.desired_state
 
-resource "aws_sfn_state_machine" "this" {
-  name     = var.name
-  role_arn = aws_iam_role.step_functions.arn
+  source = var.source_arn
+  source_parameters {
+    dynamic "sqs_queue_parameters" {
+      for_each = var.source_parameters.sqs != null ? [var.source_parameters.sqs] : []
+      content {
+        batch_size                         = sqs_queue_parameters.value.batch_size
+        maximum_batching_window_in_seconds = sqs_queue_parameters.value.maximum_batching_window_in_seconds
+      }
+    }
 
-  definition = var.definition
+    dynamic "dynamo_db_stream_parameters" {
+      for_each = var.source_parameters.dynamodb != null ? [var.source_parameters.dynamodb] : []
+      content {
+        starting_position = dynamo_db_stream_parameters.value.starting_position
+        batch_size        = dynamo_db_stream_parameters.value.batch_size
+      }
+    }
 
-  type = var.type
-
-  dynamic "logging_configuration" {
-    for_each = var.logging_configuration != null ? [var.logging_configuration] : []
-
-    content {
-      log_destination        = "${logging_configuration.value.log_group_arn}:*"
-      include_execution_data = logging_configuration.value.include_execution_data
-      level                  = logging_configuration.value.level
+    dynamic "kinesis_stream_parameters" {
+      for_each = var.source_parameters.kinesis != null ? [var.source_parameters.kinesis] : []
+      content {
+        starting_position = kinesis_stream_parameters.value.starting_position
+        batch_size        = kinesis_stream_parameters.value.batch_size
+      }
     }
   }
 
-  dynamic "tracing_configuration" {
-    for_each = var.tracing_enabled ? [true] : []
+  target = var.target_arn
+  target_parameters {
+    dynamic "step_function_state_machine_parameters" {
+      for_each = var.target_parameters.sfn != null ? [var.target_parameters.sfn] : []
+      content {
+        invocation_type = step_function_state_machine_parameters.value.invocation_type
+      }
+    }
 
-    content {
-      enabled = true
+    dynamic "lambda_function_parameters" {
+      for_each = var.target_parameters.lambda != null ? [var.target_parameters.lambda] : []
+      content {
+        invocation_type = lambda_function_parameters.value.invocation_type
+      }
+    }
+
+    dynamic "sqs_queue_parameters" {
+      for_each = var.target_parameters.sqs != null ? [var.target_parameters.sqs] : []
+      content {
+        message_group_id = sqs_queue_parameters.value.message_group_id
+      }
+    }
+
+    dynamic "event_bridge_event_bus_parameters" {
+      for_each = var.target_parameters.eventbus != null ? [var.target_parameters.eventbus] : []
+      content {
+        detail_type = event_bridge_event_bus_parameters.value.detail_type
+        source      = event_bridge_event_bus_parameters.value.source
+      }
     }
   }
 
-  tags = merge(
-    var.tags,
-    {
-      "ManagedBy" = "Terraform"
-    },
-  )
-}
+  dynamic "log_configuration" {
+    for_each = var.log_group_arn != null ? [1] : []
+    content {
+      log_level = var.log_level
+      cloudwatch_logs_log_destination {
+        log_group_arn = var.log_group_arn
+      }
+    }
+  }
 
-################################################################################
-# CloudWatch Log Group (optional)
-################################################################################
-
-resource "aws_cloudwatch_log_group" "this" {
-  count = var.create_log_group ? 1 : 0
-
-  name              = "/aws/states/${var.name}"
-  retention_in_days = var.log_retention_in_days
-  kms_key_id        = var.kms_key_id
-
-  tags = var.tags
+  tags = local.tags
 }
