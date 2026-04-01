9cd7bd76b13dc1c39a7d817b436ec8050cf8f553
Wesley Costa <98036757+FWesleycosta@users.noreply.github.com>
2026-04-01 08:56:48 -0300

    Add S3 bucket lifecycle configuration resource

---
+++ mains.tf
@@ -0,0 +1,44 @@
+variable "lifecycle_rules" {
+  description = "List of lifecycle rules for the S3 bucket"
+  type = list(object({
+    id                           = string
+    enabled                      = optional(bool, true)
+    prefix                       = optional(string, "")
+    expiration_days              = optional(number, null)
+    noncurrent_version_expiration_days = optional(number, null)
+  }))
+  default = []
+}
+
+
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+  count  = length(var.lifecycle_rules) > 0 ? 1 : 0
+  bucket = aws_s3_bucket.this.id
+
+  dynamic "rule" {
+    for_each = var.lifecycle_rules
+    content {
+      id     = rule.value.id
+      status = rule.value.enabled ? "Enabled" : "Disabled"
+
+      filter {
+        prefix = rule.value.prefix
+      }
+
+      dynamic "expiration" {
+        for_each = rule.value.expiration_days != null ? [1] : []
+        content {
+          days = rule.value.expiration_days
+        }
+      }
+
+      dynamic "noncurrent_version_expiration" {
+        for_each = rule.value.noncurrent_version_expiration_days != null ? [1] : []
+        content {
+          noncurrent_days = rule.value.noncurrent_version_expiration_days
+        }
+      }
+    }
+  }
+}
+
