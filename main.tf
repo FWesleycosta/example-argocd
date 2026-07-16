ceb497f06033a204f164adfaff9db32e3d570e16
Wesley Costa <98036757+FWesleycosta@users.noreply.github.com>
2026-07-16 15:14:06 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,121 +1,46 @@
-trigger:
-  branches:
-    include:
-      - develop
-      - sandbox
-      - hotfix/*
+locals {
+  is_public  = var.api_type == "public"  ? 1 : 0
+  is_private = var.api_type == "private" ? 1 : 0
 
-variables:
-  - name: DevEnvironment
-    type: string
-    default: 'dev'
-    displayName: 'Ambiente: dev, hml, prd'
-  - name: HmlEnvironment
-    type: string
-    default: 'hml'
-    displayName: 'Ambiente: dev, hml, prd'
-  - name: PrdEnvironment
-    type: string
-    default: 'prd'
-    displayName: 'Ambiente: dev, hml, prd'
-  - name: DevEnvironmentRegion
-    type: string
-    default: 'us-east-2'
-    displayName: 'Região: us-east-2, sa-east-1'
-  - name: HmlEnvironmentRegion
-    type: string
-    default: 'us-east-2'
-    displayName: 'Região: us-east-2, sa-east-1'
-  - name: PrdEnvironmentRegion
-    type: string
-    default: 'sa-east-1'
-    displayName: 'Região: us-east-2, sa-east-1'
+  full_domain_name = "${var.domain_internal_name}+${var.domain_name_id}"
 
+  tags = {
+    Ambiente        = var.environment
+    ManagedBy       = "Terraform"
+    Aplicacao       = var.app_name
+  }
 
-parameters:
-  - name: rollbackImageTag
-    type: string
-    default: 'none'
-    displayName: 'Rollback PRD: tag/versão a restaurar (vazio = deploy normal)'
+  sns_topics = {
+    for t in var.topic_name : t.topic_name => {
+      topic_name = "sns-${var.environment}-${data.aws_region.current.name}-${t.topic_name}${tobool(lower(t.fifo_topic)) ? ".fifo" : ""}"
+      fifo_topic = tobool(lower(t.fifo_topic))
+      content_based_deduplication = tobool(lower(t.content_based_deduplication))
+    }
+  }
 
-resources:
-  repositories:
-    - repository: templates
-      type: git
-      name: Fibra.DevOps/fibra-devops-pipelines
-      ref: refs/tags/v1.0.28
+  sqs_queues = {
+    for q in var.queue_name : q.queue_name => {
+      queue_name = "sqs-${var.environment}-${data.aws_region.current.name}-${q.queue_name}${tobool(lower(q.fifo_queue)) ? ".fifo" : ""}"
+      fifo_queue = tobool(lower(q.fifo_queue))
+    }
+  }
 
-    - repository: terraform-modules
-      type: git
-      name: Fibra.DevOps/Fibra.DevOps.Terraform
-      ref: refs/heads/main
+  managed_topic_names = toset([for t in var.topic_name : t.topic_name])
+  managed_queue_names = toset([for q in var.sqs_queues : q.queue_name])
 
-extends:
-  template: templates/stacks/dotnet-backend.yaml@templates
-  parameters:
-    rollbackImageTag: ${{ parameters.rollbackImageTag }}
+  external_topic_names = toset([
+    for s in var.sns_sqs_subscriptions : s.topic_name
+    if !contains(local.managed_topic_names, s.topic_name)
+  ])
 
-    pod:
-      min_replicas: '1'
-      max_replicas: '1'
-      requests_cpu: 100m
-      requests_memory: 256Mi
-      limits_cpu: 250m
-      limits_memory: 512Mi
-      cpu_utilization: '70'
+  external_queue_names = toset([
+    for s in var.sns_sqs_subscriptions : s.queue_name
+    if !contains(local.managed_queue_names, s.queue_name)
+  ])
 
-    networking:
-      ingress_path: /cadastro-corporativo-acl
-      base_path: cadastro-corporativo
-      api_visibility: private
-      deployment_aspnetcore_urls: http://0.0.0.0:8080
-      domain_name: dev-api-teste.bancofibra.com.br
-
-    resources:
-      dynamodb_tables: []
-      s3_buckets: []
-      secrets:
-        - name: /Cliente/CadastroCorporativo/ACL/SybaseDB
-          description: Credenciais de acesso ao banco de dados Sybase do servico ACL de Cadastro Corporativo
-          keys:
-            - username
-            - password
-            - database
-            - server
-            - port
-      sqs: 
-        - queue_name: "aws_sqs_cadastro_corporativo_sybase_acl"
-          fifo_queue: false
-        - queue_name: "aws_sqs_cadastro_corporativo_sybase_acl_dlq"
-          fifo_queue: false
-      sns_topics: []
-      sns_sqs_subscriptions:
-        - topic_name: "cliente-cadastro-corporativo-manutencao"
-          queue_name: "aws_sqs_cadastro_corporativo_sybase_acl"
-          fifo_queue: false
+  ssm_params = try(jsondecode(var.ssm_parameters), var.ssm_parameters)
+  s3_buckets = try(jsondecode(var.s3_buckets), var.s3_buckets)
 
 
-    observability:
-      dd_lang: dotnet
-      dd_lib_version: latest
-
-    config:
-      env_vars: []
-      ssm_parameters:
-        dev: 
-          - name: /Cliente/CadastroCorporativo/ACL/Sqs/QueueUrl
-            description: URL da fila SQS consumida pelo ACL de Cadastro Corporativo (dev)
-            type: String
-            value: "https://sqs.${DevEnvironmentRegion}.amazonaws.com/$(AWS_ACCOUNT_ID)/sqs-${DevEnvironment}-${DevEnvironmentRegion}-cadastro-corporativo-sybase-acl"
-        hml:
-          - name: /Cliente/CadastroCorporativo/ACL/Sqs/QueueUrl
-            description: URL da fila SQS consumida pelo ACL de Cadastro Corporativo (hml)
-            type: String
-            value: "https://sqs.${HmlEnvironmentRegion}.amazonaws.com/$(AWS_ACCOUNT_ID)/sqs-${HmlEnvironment}-${HmlEnvironmentRegion}-cadastro-corporativo-sybase-acl"
-        prd:
-          - name: /Cliente/CadastroCorporativo/ACL/Sqs/QueueUrl
-            description: URL da fila SQS consumida pelo ACL de Cadastro Corporativo (prd)
-            type: String
-            value: "https://sqs.${PrdEnvironmentRegion}.amazonaws.com/$(AWS_ACCOUNT_ID)/sqs-${PrdEnvironment}-${PrdEnvironmentRegion}-cadastro-corporativo-sybase-acl"
-
+}
  
