1582dcf28de9c4a6947f18703c7239618ce14c3e
Francisco Wesley <fwesleyper@gmail.com>
2026-08-03 14:32:05 -0300

    push

--- devops/terraform/datasource.tf
+++
@@ -1,8 +0,0 @@
-data "aws_region" "current" {}
-
-
-data "aws_lambda_layer_version" "this" {
-  for_each   = { for l in var.layers : l.name => l }
-  layer_name = each.value.name
-  version    = lookup(each.value, "version", null)
-}
